926cf06b79d5d2fd1c1a25796c28e266d1c32ce8
ps6146 <113487898+ps6146@users.noreply.github.com>
2022-09-20 15:18:23 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -3,16 +3,17 @@ void setup(){
   pinMode(LED,OUTPUT);
   digitalWrite(LED,0);
   delay(1000);
+  digitalWrite(LED,1);
 }
 
 void loop(){
   int i;
   i=0;
   while(i<5){
+    delay(100);
     digitalWrite(LED,0);
     delay(100);
     digitalWrite(LED,1);
-    delay(100);
     i=i+1;
   }
   while(1){//infinite loop
